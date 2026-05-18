7864fcc25db4298dbd6e4002f340dab0f45f3429
GabiLopes16 <12501174@aluno.cotemig.com.br>
2026-05-18 07:37:36 -0300

    Add files via upload

---
+++ aula3/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+ return render_template('home.html')
+
+@app.route('/')
+def sobre(nome):
+ return f'Olá {nome}! Seja bem vindo.'
+
+if __name__ == "__main__":
+  app.run(debug=True)
+ 
