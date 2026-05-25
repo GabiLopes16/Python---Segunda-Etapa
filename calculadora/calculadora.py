e2b5fd0fb6f69111ce89d2d5c8d1be3a96daa748
GabiLopes16 <12501174@aluno.cotemig.com.br>
2026-05-25 08:08:01 -0300

    Add files via upload

---
+++ calculadora/calculadora.py
@@ -0,0 +1,32 @@
+import requests
+from flask import Flask, render_template, request
+
+def calcular():
+ num1 = float(request.form['num1'])
+ num2 = float(request.form['num2'])
+ operacao =  request.form['operacao']
+
+ if operacao == '+':
+  resultado = num1 + num2
+  etapas = f'{num1} + {num2} = {resultado}'
+ elif operacao == '-':
+  resultado = num1 - num2
+  etapas = f'{num1} - {num2} = {resultado}'
+ elif operacao == '*':
+  resultado = num1 * num2
+  etapas = f'{num1} * {num2} = {resultado}'
+ elif operacao == '/':
+  if num2 !=0:
+   resultado = num1 / num2
+   etapas = f'{num1} / {num2} = {resultado}'
+  else: 
+   resultado = 'Erro. Divisão por 0'
+   etapas = 'Não é possível dividir por 0'
+ else:
+  resultado = 'Operação inválida'
+  etapas = 'Inválido'
+
+  return render_template('calculadora.html', etapas=etapas, resultado=resultado)
+ 
+
+
